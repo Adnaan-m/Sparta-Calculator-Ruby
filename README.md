6df6fac0734c43cb2d7f06253beb81330f75d39f
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-09-24 21:47:10 +0100

    standard and advanced cal processed with loop

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Sparta-Calculator-Ruby
+
+###Description
+The following ReadMe displays information regarding the Sparta Calculator through the use of the programming language Ruby. The Calculator provides the option for the user to select many opeations including addition, subtraction, division, multiplication, square root and to the power of specified numbers **by** the user.
+
+### Languages used
+* Ruby
+
+### How to download
+1. Search for Adnaan-m in GitHub. [Click Here](https://github.com/Adnaan-m/Sparta-Calculator-Ruby) for the link.
+2. Search for the Sparta-Calculator-Ruby repository.
+3. Download the files displayed.
+
+### Challenges
+The implication of a specific function to ask the user whether they would like to restart the calculator or quit. The code creation was simple but had to be repeated twice withi the code (once for the standard calculatior and once for the advanced calculator). A function would have reduced the lines of code making it more re-usable and neat.
+
+### Learning points
+1. Programming languages are very similar in the sense of coding but differ syntactically.
+2. Ruby is a very simplified programming language in comparison to Javascript making it more user-friendy.
--- app.rb
+++ app.rb
@@ -14,52 +14,85 @@ def Multiply num1 , num2
   totalMultiply = (num1.to_i * num2.to_i)
   puts "The Total is #{num1} x #{num2} = #{totalMultiply}"
 end
+def Power num1 , num2
+  totalPower = (num1.to_i ** num2.to_i)
+  puts "#{num1} to the power of #{num2} = #{totalPower}"
+end
+def Sqroot num1
+  totalsqrt = Math.sqrt(num1.to_i)
+  puts "√#{num1} = #{totalsqrt}"
+end
 
 puts "Welcome to BTEC Ruby calculator V2.0.1.2"
 
 puts "Here you can use our amazing standard and advanced calculators"
 
-puts "To use the Standard Calculator type 'S' OR Advanced type 'A'"
+restrtResponse = true
+while restrtResponse
 
+  puts "To use the Standard Calculator type 'S' OR Advanced type 'A'"
 
-calcType = gets.chomp
-puts calcType
+  calcType = gets.chomp
+  puts calcType
 
-if calcType == "S"
-  puts "Select your first number"
-  num1 = gets.chomp
-  puts "Select your second number"
-  num2 = gets.chomp
-  puts "Press A to add,
-  S to subtract
-  D to Divide
-  and M to multiply"
-  operatorSelection = gets.chomp
+  # IF STATEMENT FOR CALCULATOR TYPE 1
+  if calcType == "S"
+    puts "Select your first number"
+    num1 = gets.chomp
+    puts "Select your second number"
+    num2 = gets.chomp
+    puts "Press A to add,
+    S to subtract
+    D to Divide
+    and M to multiply"
+    operatorSelection = gets.chomp
 
-  if operatorSelection == "A"
-    Add num1,num2
-  elsif operatorSelection == "S"
-    Minus num1 , num2
-  elsif operatorSelection == "D"
-    Divide num1 , num2
-  elsif operatorSelection == "M"
-    Multiply num1 , num2
+    if operatorSelection == "A"
+      Add num1,num2
+    elsif operatorSelection == "S"
+      Minus num1 , num2
+    elsif operatorSelection == "D"
+      Divide num1 , num2
+    elsif operatorSelection == "M"
+      Multiply num1 , num2
+    end
+
+    # START AGAIN
+    puts "Would you like to start again?
+    Y - Yes
+    N - No"
+    restrtResp = gets.chomp
+    if restrtResp == "N"
+      restrtResponse = false
+    end
+
+    # IF STATEMENT FOR CALCULATOR TYPE 2
+  elsif calcType == "A"
+    puts "Press S to square-root OR
+    P to process the power"
+    operatorSelection = gets.chomp
+    if operatorSelection == "S"
+      puts "Select your number to square-root"
+      num1 = gets.chomp
+      Sqroot num1
+    elsif operatorSelection == "P"
+      puts "Select your first number"
+      num1 = gets.chomp
+      puts "Select your second number"
+      num2 = gets.chomp
+      Power num1,num2
+    end
+
+    # START AGAIN
+    puts "Would you like to start again?
+    Y - Yes
+    N - No"
+    restrtResp = gets.chomp
+    if restrtResp == "N"
+      restrtResponse = false
+    end
+
+    # END OF CALCULATOR TYPE SECTION
   end
-
-
-
-
-
-
-
-
-
-  # DO SOMETHING
-elsif calcType == "A"
-  # DO SOMETHING
+  #end for while loop
 end
-
-
-
-
-# √
